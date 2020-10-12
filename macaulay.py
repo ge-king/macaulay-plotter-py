260cde93396669bb6c7aafd024b3ea9e1db3a5da
George <george.pking@gmail.com>
2020-10-12 21:16:07 +0100

    Update macaulay.py
    force graph updated

--- macaulay.py
+++ macaulay.py
@@ -6,19 +6,19 @@ mag = list()
 dist = list()
 power = list()
 
-def graph_menu(x,mag,dist,power)):
+def graph_menu(x,mag,dist,power,xend,num_forces):
     print("\nWould you like to plot:")
     print("(1) Both graphs")
     print("(2) Force diagram")
     print("(3) Bending moment diagram")
     graphchoice = input("")
     if graphchoice == "1":
-        force(x,mag,dist,power))
-        bend(x,mag,dist,power))
+        force(x,mag,dist,power,xend,num_forces)
+        bend(x,mag,dist,power,xend,num_forces)
     elif graphchoice == "2":
-        force(x,mag,dist,power))
+        force(x,mag,dist,power,xend,num_forces)
     elif graphchoice == "3":
-        bend(x,mag,dist,power))
+        bend(x,mag,dist,power,xend,num_forces)
     else:
         print("Error - choose from 1-3.")
         graph_menu()
@@ -38,15 +38,26 @@ def main_menu():
 
     print("\nWhere would you like the graph to end along the x axis?")
     xend = int(input(""))
-    x = np.linspace(0,xend,num=100)
-    print(x)
-    graph_menu(x,mag,dist,power))
+    graph_menu(x,mag,dist,power,xend,num_forces)
 
-def force(x,mag,dist,power):
-    print(x)
-    y = np.linspace()
-    plt.plot(x,)
-    plt.show()
+def force(x,mag,dist,power,xend,num_forces):
+    print(dist)
+    if dist[0] != 0:
+        x = np.linspace(0,dist[0],num=50)
+        y = 0*x
+        plt.plot(x,y)
+        if power[0] == 0:
+            y = np.linspace(0,mag[0],num=50)
+            x = 0*y+dist[0]
+            plt.plot(x,y)
+    for k in range(0,num_forces-1):
+        x = np.linspace(dist[k],dist[k+1],num=50)
+        y = mag[k]*(x-dist[k])**power[k]
+        plt.plot(x,y)
+    x = np.linspace(dist[num_forces-1],xend,num=50)
+    y = mag[num_forces-1]*(x-dist[num_forces-1])**power[num_forces-1]
+    plt.plot(x,y)
+    plt.show()    
 
 
 main_menu()
